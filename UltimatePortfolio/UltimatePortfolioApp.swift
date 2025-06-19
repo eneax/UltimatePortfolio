de4a90472e14ec27a06b30c7096f5fb435f41089
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-06-19 17:22:58 +0200

    Initial commit

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,21 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Enea Xharja on 17.06.25.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    @StateObject var dataController = DataController()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+                .environmentObject(dataController)
+        }
+    }
+}
