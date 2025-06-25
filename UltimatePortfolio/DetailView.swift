9c8ed05bcbc66df61afeebdef8ce0e5b5b2bca79
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-06-25 10:42:59 +0200

    Add NavigationSplitView with three views - sidebar, content and details

---
+++ UltimatePortfolio/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  UltimatePortfolio
+//
+//  Created by Enea Xharja on 25.06.25.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("Detail")
+    }
+}
+
+#Preview {
+    DetailView()
+}
---
+++ UltimatePortfolio/SidebarView.swift
@@ -0,0 +1,18 @@
+//
+//  SidebarView.swift
+//  UltimatePortfolio
+//
+//  Created by Enea Xharja on 25.06.25.
+//
+
+import SwiftUI
+
+struct SidebarView: View {
+    var body: some View {
+        Text("Sidebar")
+    }
+}
+
+#Preview {
+    SidebarView()
+}
