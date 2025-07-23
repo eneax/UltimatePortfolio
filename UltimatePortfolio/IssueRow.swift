c345cb2154c07bcab7b0cf7da3dbe1c5c99c4b1d
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-07-23 16:26:57 +0200

    Add i18n support

--- UltimatePortfolio/IssueRow.swift
+++ UltimatePortfolio/IssueRow.swift
@@ -31,7 +31,7 @@ struct IssueRow: View {
                 Spacer()
                 
                 VStack(alignment: .trailing) {
-                    Text(issue.issueCreationDate.formatted(date: .abbreviated, time: .omitted))
+                    Text(issue.issueFormattedCreationDate)
                         .accessibilityLabel(issue.issueCreationDate.formatted(date: .abbreviated, time: .omitted))
                         .font(.subheadline)
 
