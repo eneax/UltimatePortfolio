38f305525669cb875b156a3deee8592c3819e6a2
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-07-30 13:43:32 +0200

    Refactor SidebarView to use SmartFilterRow and UserFilterRow components

---
+++ UltimatePortfolio/SmartFilterRow.swift
@@ -0,0 +1,22 @@
+//
+//  SmartFilterRow.swift
+//  UltimatePortfolio
+//
+//  Created by Enea Xharja on 29.07.25.
+//
+
+import SwiftUI
+
+struct SmartFilterRow: View {
+    var filter: Filter
+    
+    var body: some View {
+        NavigationLink(value: filter) {
+            Label(LocalizedStringKey(filter.name), systemImage: filter.icon)
+        }
+    }
+}
+
+#Preview {
+    SmartFilterRow(filter: .all)
+}
---
+++ UltimatePortfolio/UserFilterRow.swift
@@ -0,0 +1,41 @@
+//
+//  UserFilterRow.swift
+//  UltimatePortfolio
+//
+//  Created by Enea Xharja on 29.07.25.
+//
+
+import SwiftUI
+
+struct UserFilterRow: View {
+    var filter: Filter
+    var rename: (Filter) -> Void
+    var delete: (Filter) -> Void
+    
+    var body: some View {
+        NavigationLink(value: filter) {
+            Label(filter.name, systemImage: filter.icon)
+                .badge(filter.activeIssuesCount)
+                .contextMenu {
+                    Button {
+                        rename(filter)
+                    } label: {
+                        Label("Rename", systemImage: "pencil")
+                    }
+                    
+                    Button(role: .destructive) {
+                        delete(filter)
+                    } label: {
+                        Label("Delete", systemImage: "trash")
+                    }
+                }
+                .accessibilityElement()
+                .accessibilityLabel(filter.name)
+                .accessibilityHint("\(filter.activeIssuesCount) issues")
+        }
+    }
+}
+
+#Preview {
+    UserFilterRow(filter: .all, rename: { _ in }, delete: { _ in})
+}
