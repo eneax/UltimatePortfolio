24ec0767315f8d0046fb1555c07703f91b811994
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-08-30 14:17:48 +0200

    Add tests to verify deleting tag does not delete issues

--- UltimatePortfolioTests/TagTests.swift
+++ UltimatePortfolioTests/TagTests.swift
@@ -12,7 +12,7 @@ import XCTest
 final class TagTests: BaseTestCase {
     func testCreatingTagsAndIssues() {
         let count = 10
-        let issues = count * count
+        let issueCount = count * count
 
         for _ in 0..<count {
             let tag = Tag(context: managedObjectContext)
@@ -23,7 +23,19 @@ final class TagTests: BaseTestCase {
             }
         }
 
-        XCTAssertEqual(dataController.count(for: Tag.fetchRequest()), count, "Expected tags: \(count).")
-        XCTAssertEqual(dataController.count(for: Issue.fetchRequest()), issues, "Expected issues: \(issues).")
+        XCTAssertEqual(dataController.count(for: Tag.fetchRequest()), count, "Expected \(count) tags.")
+        XCTAssertEqual(dataController.count(for: Issue.fetchRequest()), issueCount, "Expected \(issueCount) issues.")
+    }
+
+    func testDeletingTagDoesNotDeleteIssues() throws {
+        dataController.createSampleData()
+
+        let request = NSFetchRequest<Tag>(entityName: "Tag")
+        let tags = try managedObjectContext.fetch(request)
+
+        dataController.delete(tags[0])
+
+        XCTAssertEqual(dataController.count(for: Tag.fetchRequest()), 4, "Expected 4 tags after deleting 1.")
+        XCTAssertEqual(dataController.count(for: Issue.fetchRequest()), 50, "Expected 50 issues after deleting a tag.")
     }
 }
