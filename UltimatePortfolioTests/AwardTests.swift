b019b5b83838e145a6a9fad6500fa45fdc137e8f
Enea Xharja <19818937+eneax@users.noreply.github.com>
2025-08-30 14:53:11 +0200

    Add test for unlocking awards when closing issues

--- UltimatePortfolioTests/AwardTests.swift
+++ UltimatePortfolioTests/AwardTests.swift
@@ -43,4 +43,28 @@ final class AwardTests: BaseTestCase {
             dataController.deleteAll()
         }
     }
+
+    func testClosedAwards() {
+        let values = [1, 10, 20, 50, 100, 250, 500, 1000]
+
+        for (count, value) in values.enumerated() {
+            var issues = [Issue]()
+
+            for _ in 0..<value {
+                let issue = Issue(context: managedObjectContext)
+                issue.completed = true
+                issues.append(issue)
+            }
+
+            let matches = awards.filter { award in
+                award.criterion == "closed" && dataController.hasEarned(award: award)
+            }
+
+            XCTAssertEqual(matches.count, count + 1, "Completing \(value) issues should unlock \(count + 1) awards.")
+
+            for issue in issues {
+                dataController.delete(issue)
+            }
+        }
+    }
 }
